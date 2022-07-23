26323543442c55840d1ed07c388b95c3b5bf7d07
mkent-at-loginvsi <m.kent@loginvsi.com>
2022-07-23 16:49:31 -0700

    Update build.sh
    curl syntax...duh.

--- build/build.sh
+++ build/build.sh
@@ -10,16 +10,22 @@ if [[ $FREE -lt 39062500 ]]; then               # 40G = 26*1024*1024k (Kibibyte)
 fi
 
 # Create Build Directory
+echo "--------------------------------"
 echo "Creating Build Directory"
+echo "--------------------------------"
 dir="build-$(date +%Y_%m_%d_%H_%M_%S)"
 export BUILD_DIR="$PWD/$dir"
 output="appliance"
 sudo mkdir $dir
+echo "--------------------------------"
 echo "Relative Build Direcory: $dir"
 echo "Full Path Build Directory: $BUILD_DIR"
+echo "--------------------------------"
 
 # Download VHD
-echo "Downloading Virtual Appliance"
+echo "--------------------------------"
+echo "Downloading Virtual Appliance to $BUILD_DIR/$applianceFile"
+echo "--------------------------------"
 applianceFile="AZ-VA-LoginEnterprise-4.8.10.zip"
 if ! [ -f $BUILD_DIR/$applianceFile ]; then
   sudo curl -O "https://loginvsidata.s3.eu-west-1.amazonaws.com/LoginEnterprise/VirtualAppliance/$applianceFile" -o "$BUILD_DIR/$applianceFile"
@@ -27,14 +33,18 @@ fi
 
 
 # Unzip VHD
+echo "--------------------------------"
 echo "Unzipping Virtual Appliance VHD"
+echo "--------------------------------"
 sudo yum install -y unzip
 if ! [ -f $BUILD_DIR/$applianceFile ]; then
   sudo unzip $BUILD_DIR/$applianceFile
 fi
 
 # Mount VHD
+echo "--------------------------------"
 echo "Mounting Virtual Hard Drive"
+echo "--------------------------------"
 sudo yum install -y libguestfs-tools
 sudo mkdir /mnt/vhd
 mountpath="$BUILD_DIR"
@@ -42,7 +52,9 @@ export LIBGUESTFS_BACKEND=direct
 sudo guestmount --add $mountpath/AZ-VA-LoginEnterprise-4.8.10.vhd --ro /mnt/vhd/ -m /dev/sda1
 
 # Fail if VHD doesn't exist
+echo "--------------------------------"
 echo "Checking if VHD Mounted"
+echo "--------------------------------"
 if ! [ -f /mnt/vhd/loginvsi ]; then
   echo "Mount failed"
   return 1
