b7f9993dfda301fd47a53e8c3edc34a03e78a430
mkent-at-loginvsi <m.kent@loginvsi.com>
2022-07-23 22:54:31 -0700

    Update build.sh
    fixed perms, removed sudo

--- build/build.sh
+++ build/build.sh
@@ -20,7 +20,7 @@ echo "----------------------------------------------------------------"
 dir="build-$(date +%Y_%m_%d_%H_%M_%S)"
 export BUILD_DIR="$PWD/$dir"
 out_dir="appliance"
-sudo mkdir $dir
+mkdir $dir
 echo "----------------------------------------------------------------"
 echo "Relative Build Direcory: $dir"
 echo "Full Path Build Directory: $BUILD_DIR"
@@ -33,18 +33,18 @@ echo "Downloading Virtual Appliance to $BUILD_DIR/$applianceFile"
 echo "----------------------------------------------------------------"
 
 # Shortcut for testing...if the appliance zip is one level up, copy it
-if [ -f ../$BUILD_DIR/$applianceFile ]; then
-  echo "copying ../$BUILD_DIR/$applianceFile"
-  sudo cp ../$BUILD_DIR/$applianceFile $BUILD_DIR
-fi
+#if [ -f ../$BUILD_DIR/$applianceFile ]; then
+#  echo "copying ../$BUILD_DIR/$applianceFile"
+#  cp ../$BUILD_DIR/$applianceFile $BUILD_DIR
+#fi
 
-if [ -f $applianceFile ]; then
-  echo "copying $applianceFile"
-  sudo cp $applianceFile $BUILD_DIR
-fi
+#if [ -f $applianceFile ]; then
+#  echo "copying $applianceFile"
+#  cp $applianceFile $BUILD_DIR
+#fi
 
 if ! [ -f $BUILD_DIR/$applianceFile ]; then
-  sudo curl -o $BUILD_DIR/$applianceFile https://loginvsidata.s3.eu-west-1.amazonaws.com/LoginEnterprise/VirtualAppliance/$applianceFile
+  curl -o $BUILD_DIR/$applianceFile https://loginvsidata.s3.eu-west-1.amazonaws.com/LoginEnterprise/VirtualAppliance/$applianceFile
 fi
 
 
@@ -55,7 +55,7 @@ echo "Unzipping Virtual Appliance VHD $BUILD_DIR/$applianceFile"
 echo "----------------------------------------------------------------"
 sudo yum install -y unzip
 if ! [ -f $BUILD_DIR/"${applianceFile/zip/vhd}" ]; then
-  sudo unzip -d $BUILD_DIR $BUILD_DIR/$applianceFile
+  unzip -d $BUILD_DIR $BUILD_DIR/$applianceFile
 fi
 
 # Mount VHD
@@ -69,7 +69,7 @@ sudo chmod 777 /mnt/vhd
 mountpath="$BUILD_DIR"
 LIBGUESTFS_BACKEND=direct
 export LIBGUESTFS_BACKEND
-echo $LIBGUESTFS_BACKEND
+
 guestmount --add $mountpath/AZ-VA-LoginEnterprise-4.8.10.vhd --ro /mnt/vhd/ -m /dev/sda1
 
 # Fail if VHD doesn't exist
@@ -83,30 +83,31 @@ fi
 
 # Copy Files and Directories to output dir
 build_out=$BUILD_DIR/$out_dir
-sudo mkdir $build_out
+mkdir $build_out
+#sudo chmod 777 $build_out
 
 # Copy Login Enterprise Installation
-sudo cp -r /mnt/vhd/loginvsi $build_out/
+cp -r /mnt/vhd/loginvsi $build_out/
 
 #Copy Login Enterprise Service
-sudo mkdir -p $build_out/etc/systemd/system/
-sudo cp -f /mnt/vhd/etc/systemd/system/loginvsid.service $build_out/etc/systemd/system/loginvsid.service
+mkdir -p $build_out/etc/systemd/system/
+cp -f /mnt/vhd/etc/systemd/system/loginvsid.service $build_out/etc/systemd/system/loginvsid.service
 
 #Copy Login Enterprise Service Watcher
-sudo cp -f /mnt/vhd/etc/systemd/system/pi_guard.service $build_out/mnt/vhd/etc/systemd/system/pi_guard.service
+cp -f /mnt/vhd/etc/systemd/system/pi_guard.service $build_out/etc/systemd/system/pi_guard.service
 
 #Copy hidden files
-sudo mkdir -p $build_out/root
+#mkdir -p $build_out/root
 #sudo cp -f /mnt/vhd/root/.play output/root/.play
 
 #Copy firstrun, daemon and Menuing
-sudo mkdir -p $build_out/usr/bin
-sudo cp -f /mnt/vhd/usr/bin/loginvsid $build_out/usr/bin/loginvsid
-sudo curl -O https://github.com/mkent-at-loginvsi/rhel-install/raw/main/pdmenu/pdmenu.rhel
-sudo cp -f pdmenu $build_out/usr/bin/
+mkdir -p $build_out/usr/bin
+cp -f /mnt/vhd/usr/bin/loginvsid $build_out/usr/bin/loginvsid
+curl -o $build_out/usr/bin/pdmenu https://github.com/mkent-at-loginvsi/rhel-install/raw/main/pdmenu/pdmenu.rhel
+#cp -f pdmenu $build_out/usr/bin/
 
 #zip up appliance build
-sudo tar -czvf $out_dir.tar.gz $build_out/*
+tar -czvf $out_dir.tar.gz $build_out/*
 
 #Unmount vhd
 sudo guestunmount /mnt/vhd
